2e17064740b61bdf4bfe557d69d26905bcb442eb
Mohammad Amir Ansari <aamiransari92111721@gmail.com>
2025-10-14 10:12:58 +0530

    new

--- cpp/prime.cpp
+++ cpp/prime.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int prime(int n, int i = 2)
+{
+    if (n % i == 0)
+        return false;
+    else if (n <= 2 || i * i > n)
+        return true;
+    return prime(n, ++i);
+}
+
 int main()
 {
     int n, flag = 1;
     cin >> n;
+
+    cout << prime(n);
 
     if (n <= 1)
         cout << "Composite";
