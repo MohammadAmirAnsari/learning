5d6038cdbf8778340eaf1644396d8e78cf895fb8
Mohammad Amir Ansari <aamiransari92111721@gmail.com>
2025-11-03 22:31:59 +0530

    new

---
+++ cpp/strupper.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "BacDeF";
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (int(s[i]) >= 97 && int(s[i]) <= 122)
+        {
+            s[i] = char(s[i] - 32);
+        }
+        cout << s[i];
+    }
+    return 0;
+}
